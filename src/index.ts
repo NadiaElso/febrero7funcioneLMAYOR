bdbcad81c935a6967b927c9f4298034079de64b1
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-02-07 20:02:39 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function elMayor(numero1:number,numero2:number) {
+  if (numero1>numero2) {
+    console.log("El número mayor es"  + numero1);
+  } else{
+    console.log("El número mayor es"  + numero2);
+
+  }
+  
+}
+ elMayor(6,3)
